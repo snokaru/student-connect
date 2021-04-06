ee17fb76f872f2c9d1e13400fd047c41ba2fc0ad
Vlad Slivilescu <vlad.slivilescu@gmail.com>
2021-04-06 12:20:43 +0300

    Add middleware for user update

--- utils/middleware.js
+++ utils/middleware.js
@@ -1,5 +1,8 @@
 const config = require("../utils/config");
+const logger = require("../utils/logger");
 const jwt = require("jsonwebtoken");
+
+const User = require("../models/user");
 
 const tokenExtractor = (req, res, next) => {
     const token = req.header("Authorization");
@@ -14,6 +17,41 @@ const tokenExtractor = (req, res, next) => {
     next();
 };
 
+const userUpdater = async (request, response, next) => {
+
+    logger.info("In user updater!")
+
+    const { name, address, description, student, company } = request.body;
+    if (student && company) {
+        request.status(400).json({ "error": "can't update both company and student info" });
+    }
+    try {
+        let searchedUser = await User.findById(request.user.id);
+        logger.info(searchedUser);
+        searchedUser.name = name === undefined ? searchedUser.name : name; 
+        searchedUser.address = address === undefined ? searchedUser.address : address;
+        searchedUser.description = description === undefined ? serachedUser.description : description;
+
+        if (student && searchedUser.student) {
+            searchedUser.student.birthDate = student?.birthDate === undefined ? searchedUser.student?.birthDate : student?.birthDate;
+            searchedUser.student.school = student?.school === undefined ? searchedUser.student?.school : student?.school;
+        }
+        else if (company && searchedUser.company) {
+            searchedUser.company.creationDate = company?.creationDate === undefined ? searchedUser.company?.creationDate : company?.creationDate;
+            searchedUser.company.activity = company?.activity === undefined ? searchedUser.company?.activity : company?.activity;
+        }
+
+        await searchedUser.save();
+        response.json(searchedUser);
+    } catch (error) {
+        logger.info(error);
+        response.status(400).json({ "error": "no such user found!" });
+    }
+
+    next();
+};
+
 module.exports = {
     tokenExtractor,
+    userUpdater,
 };
