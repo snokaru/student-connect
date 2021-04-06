5717fedb0599b601768ac9b2c45d414ebd06c16f
Vlad Slivilescu <vlad.slivilescu@gmail.com>
2021-04-06 21:54:34 +0300

    Add React Router package and basic routing
    This patch aims to provide a basic React Router framework. Routing of
    Login, Register and Home pages was added.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,35 @@
 import React from 'react'
+import LoginForm from "./components/LoginForm/LoginForm";
 import RegisterForm from './components/RegisterForm/RegisterForm';
 import UserState from './components/UserState/UserState';
 import Home from './components/Pages/Home';
 import NavBar from './components/Layout/Navbar/Navbar';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+
+
 const App = () => {
+
   return (
-    <React.Fragment>
-      <UserState>
-      <NavBar></NavBar>
-      <Home/>
-      <RegisterForm/>
-      </UserState>
-    </React.Fragment>
+    <UserState>
+      <Router>
+        <NavBar />
+        <Switch>
+          <Route path="/login">
+            <LoginForm />
+          </Route>
+          <Route path="/register">
+            <RegisterForm />
+          </Route>
+          <Route path="/">
+            <Home/>
+          </Route>
+        </Switch>
+      </Router>
+    </UserState>
   );
 };
 
--- frontend/src/components/Layout/Navbar/Navbar.js
+++ frontend/src/components/Layout/Navbar/Navbar.js
@@ -1,12 +1,14 @@
-import React from 'react'
+import React from 'react';
 import classes from './Navbar.module.css';
+import { NavLink } from "react-router-dom";
 
 export const Navbar = () => {
     return (
         <div className={classes.topnav} id="myTopnav">
-  <a className={classes.active}>Home</a>
-  <a>Conectati-ma</a>
-</div>
-    )
+            <NavLink exact to="/" activeClassName={classes.active}>Home</NavLink> 
+            <NavLink exact to="/login" activeClassName={classes.active}>Log In</NavLink>
+            <NavLink exact to="/register" activeClassName={classes.active}>Register</NavLink>
+        </div>
+    );
 }
-export default Navbar
+export default Navbar;
