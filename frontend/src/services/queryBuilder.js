c2342cfa95f70e72d395b061bb550f7707cc5ecc
Vlad Slivilescu <vlad.slivilescu@gmail.com>
2021-04-16 21:26:41 +0300

    Add query builder abstraction class
    This patch aims to provide an abstraction layer for querying the
    backend, with interfaces for filtering, sorting and paginating the
    backend data

---
+++ frontend/src/services/queryBuilder.js
@@ -0,0 +1,35 @@
+import axios from "axios";
+import { BASE_URL } from "../utils/config";
+
+/*
+const query = new QueryBuilder().filter('email' 'abcd@gmail.com').filter(...).paginate().sort().exec()
+*/
+
+class QueryBuilder {
+    constructor(resourceRoute) {
+        this.baseUrl = `${BASE_URL}/${resourceRoute}?`;
+        return this;
+    }
+
+    filter(field, value) {
+        this.baseUrl += `&${field}=${value}`;
+        return this;
+    }
+
+    paginate(page, limit) {
+        this.baseUrl += `&page=${page}&limit=${limit}`;
+        return this;
+    }
+
+    sort(criteria, isAsc) {
+        this.baseUrl += `&sort_by=${isAsc ? "" : "-"}${criteria}`;
+        return this;
+    }
+
+    async exec() {
+        const response = await axios.get(this.baseUrl);
+        return response.data.docs;
+    }
+}
+
+export default QueryBuilder;
