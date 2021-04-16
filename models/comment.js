d1a650d03ad9b39ccdcfefb13dddc648e895e16a
RobertDragoi <robertdragoi20@gmail.com>
2021-04-16 10:50:21 +0300

    Fix comment model

--- models/comment.js
+++ models/comment.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 const commentSchema = new mongoose.Schema({
-  post: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Post",
+  createdAt: {
+    type: Date,
+    default: Date.now(),
   },
   user: {
     type: mongoose.Schema.Types.ObjectId,
