b2703377f346a3fa2bc5335e7734b81b22e7ecef
RobertDragoi <robertdragoi20@gmail.com>
2021-04-26 15:51:12 +0300

    Add delete button to user's posts

--- frontend/src/components/Layout/Job.js
+++ frontend/src/components/Layout/Job.js
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useContext } from "react";
 import ReactImageFallback from "react-image-fallback";
 import { BASE_URL } from "../../utils/config";
 import { Link } from "react-router-dom";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
+import UserContext from "../UserState/userContext";
 const Job = (props) => {
+  const userContext = useContext(UserContext);
+  const { user } = userContext;
   const formatDate = (date) => {
     if (!date) {
       return "Not set";
@@ -26,19 +30,34 @@ const Job = (props) => {
       <div className={`card m-3 mx-auto ${props.className}`}>
         <div className="card-body d-flex flex-column justify-content-between">
           <div className="card-title d-flex align-items-center">
-            <ReactImageFallback
-              src={props.companyPicture}
-              fallbackImage={`${BASE_URL}/public/img/default.jpg`}
-              alt="profile"
-              width="50"
-              height="50"
-            />
-            <p className="card-text m-0 ml-3 d-flex flex-column">
-              <span>
-                <Link to={`/users/${props.user.id}`}>{props.userName}</Link>
-              </span>
-              <span className="text-muted">{formatDate(props.when)}</span>
-            </p>
+            <div className="p-1">
+              <ReactImageFallback
+                src={props.companyPicture}
+                fallbackImage={`${BASE_URL}/public/img/default.jpg`}
+                alt="profile"
+                width="50"
+                height="50"
+              />
+            </div>
+            <div className="p-1">
+              <p className="card-text m-0 ml-3 d-flex flex-column">
+                <span>
+                  <Link to={`/users/${props.user.id}`}>{props.user.name}</Link>
+                </span>
+                <span className="text-muted">{formatDate(props.when)}</span>
+              </p>
+            </div>
+            <div className="ml-auto p-1">
+              {user.id === props.user.id ? (
+                <span>
+                  <button type="button" class="btn btn-outline-primary">
+                    <FontAwesomeIcon icon={faTrash} />
+                  </button>
+                </span>
+              ) : (
+                <React.Fragment />
+              )}
+            </div>
           </div>
           <Link to={`/post/${props.id}`}>
             <p className="card-title text-primary d-inline">{props.title}</p>
