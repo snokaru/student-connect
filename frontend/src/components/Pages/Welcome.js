3c7b5d917227c183e86d0b87ad3482f9497f8c51
CasianGalben <casian.galben00@e-uvt.ro>
2021-05-23 17:49:01 +0300

    Test welcome page

--- frontend/src/components/Pages/Welcome.js
+++ frontend/src/components/Pages/Welcome.js
@@ -2,9 +2,19 @@ import React from 'react'
 
 export const Welcome = () => {
     return (
-        <div>
-            
-        </div>
+        <div class="container">
+  <div class="row">
+    <div class="col-sm">
+      One of three columns
+    </div>
+    <div class="col-sm">
+      One of three columns
+    </div>
+    <div class="col-sm">
+      One of three columns
+    </div>
+  </div>
+</div>
     )
 }
 
